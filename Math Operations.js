43918ffa78c36eb6bcf780d95560ffeb8e2ce3c9
André Lucas de Assis Silva <91093717+assisandre@users.noreply.github.com>
2022-10-27 10:48:35 -0300

    Add files via upload

---
+++ Math Operations.js
@@ -0,0 +1,11 @@
+// Math operations
+var a = 5;
+var b = 10;
+var c = a + b;
+var d = b - a;
+var e = a * b;
+var f = b / 2;
+
+console.log("a + b = " + c);
+console.log("b - a  = " + d);
+console.log("a * b  = " + e);
